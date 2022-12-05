f6e0957e81949187777c4de6da0bed7f00fd8640
Raf Ceuls <rceuls@gmail.com>
2022-12-05 09:23:05 +0100

    day 5 with static crate setup

---
+++ day05.spec.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
+import { generateDayFiveResult } from "./day05.ts";
+
+const TEST_INPUT = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+Deno.test("Day 04 Part 01 + 02", () => {
+  const calculated = generateDayFiveResult({
+    1: "NZ".split("").reverse(),
+    2: "DCM".split("").reverse(),
+    3: ["P"]
+  }, TEST_INPUT);
+  assertEquals(calculated.part1, "CMZ");
+  assertEquals(calculated.part2, "MCD");
+});
--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { generateDayOneResult } from "./day01.ts";
 import { generateDayTwoResult } from "./day02.ts";
 import { generateDayThreeResult } from "./day03.ts";
 import { generateDayFourResult } from "./day04.ts";
+import { generateDayFiveResult, staticCrates } from "./day05.ts";
 
 import { readAsText } from "./shared.ts";
 
@@ -9,7 +10,9 @@ const DAY_INPUT = {
   day01: await readAsText("./day01.input.txt"),
   day02: await readAsText("./day02.input.txt"),
   day03: await readAsText("./day03.input.txt"),
-  day04: await readAsText("./day04.input.txt")
+  day04: await readAsText("./day04.input.txt"),
+  day05: await readAsText("./day05.input.txt")
+
 };
 
 const SHOULD_LOG_EXTENDED = Deno.env.get("VERBOSE") === "true";
@@ -27,6 +30,8 @@ if (import.meta.main) {
   logIntermediateResults(generateDayTwoResult(DAY_INPUT.day02));
   logIntermediateResults(generateDayThreeResult(DAY_INPUT.day03));
   logIntermediateResults(generateDayFourResult(DAY_INPUT.day04));
+  logIntermediateResults(generateDayFiveResult(staticCrates(), DAY_INPUT.day05));
+  // wrong: SQMQZRFGF
   console.timeEnd("TOTAL");
 
 }
